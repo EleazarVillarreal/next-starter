1e3b9233377ba66d686671a7bff5c33fcac4056b
Eleazar Villarreal <EleazarVillarreal13@gmail.com>
2025-02-23 21:00:44 -0600

    eslint and prettier configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Unable to use with TurboPack
+  // experimental: {
+  //   typedRoutes: true
+  // }
 }
 
 export default nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+
 import '../styles/globals.css'
 
 export const metadata: Metadata = {
@@ -15,8 +16,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link
-          rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
+          rel="icon"
         />
       </head>
       <body className={`antialiased`}>{children}</body>
