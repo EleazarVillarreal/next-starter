420ce7331d9c2fad377d8d79a66b8f60b6b7e4cd
Eleazar Villarreal <EleazarVillarreal13@gmail.com>
2025-03-03 20:57:02 -0600

    added base font to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
 import '@/styles/globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter'
+})
 
 export const metadata: Metadata = {
   title: 'Next Starter Project',
@@ -20,7 +26,7 @@ export default function RootLayout({
           rel="icon"
         />
       </head>
-      <body className={`antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   )
 }
