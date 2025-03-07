92df87a5e6ab9c296a2c0ae075aa39960bd91bad
Eleazar Villarreal <EleazarVillarreal13@gmail.com>
2025-03-07 13:21:54 -0600

    updated root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { IconPackage } from '@tabler/icons-react'
-
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <IconPackage />
-      <h1>Next.js Starter Project</h1>
-    </div>
+    <main className="mx-auto flex h-dvh max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8">
+      <h1 className="bg-gradient-to-r from-[--primary] to-[--accent] bg-clip-text text-2xl font-semibold leading-tight tracking-tight text-transparent md:text-4xl">
+        Next.js Starter Project
+      </h1>
+    </main>
   )
 }
