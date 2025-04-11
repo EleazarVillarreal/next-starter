5125c3dc19e105b23baf2451b31a17ad0e733192
Eleazar Villarreal <EleazarVillarreal13@gmail.com>
2025-04-11 13:47:03 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="mx-auto flex h-dvh max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8">
+      <h1>Next.js Starter Project</h1>
+    </main>
+  )
+}
