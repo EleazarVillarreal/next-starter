fb4657f925653e2d986761d0d55e8982b45e5b39
Eleazar Villarreal <EleazarVillarreal13@gmail.com>
2025-04-12 18:30:58 -0500

    added custom colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="mx-auto flex h-dvh max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8">
-      <h1>Next.js Starter Project</h1>
+      <h1 className="bg-gradient-to-r from-(--primary) to-(--accent) bg-clip-text text-3xl leading-tight font-semibold tracking-tight text-transparent md:text-6xl">
+        Next.js Starter Project
+      </h1>
     </main>
   )
 }
